da7d181f0985aa973f83d2342f06fbae8cdfec9d
Lautaro Iraneta <Lautaro.Iraneta@thomsonreuters.com>
2019-09-27 01:49:29 -0300

    Agregado multiselect dropdown y pantalla materia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CrearPerfilComponent } from './crear-perfil/crear-perfil.component';
 import { AlumnoComponent } from './alumno/alumno.component';
 import { CarreraComponent } from './carrera/carrera.component';
 import { EmpresaComponent } from './empresa/empresa.component';
+import { MateriaComponent } from './materia/materia.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/crear-perfil', pathMatch: 'full' },
   { path: 'alumno', component: AlumnoComponent },
   { path: 'carrera', component: CarreraComponent },
   { path: 'crear-perfil', component: CrearPerfilComponent },
-  { path: 'empresa', component: EmpresaComponent }
+  { path: 'empresa', component: EmpresaComponent },
+  { path: 'materia', component: MateriaComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule }    from '@angular/common/http';
 
 import { DatepickerModule, BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -13,6 +14,7 @@ import { CrearPerfilComponent } from './crear-perfil/crear-perfil.component';
 import { AlumnoComponent } from './alumno/alumno.component';
 import { CarreraComponent } from './carrera/carrera.component';
 import { EmpresaComponent } from './empresa/empresa.component';
+import { MateriaComponent } from './materia/materia.component';
 
 @NgModule({
   imports: [
@@ -23,14 +25,16 @@ import { EmpresaComponent } from './empresa/empresa.component';
     BsDatepickerModule.forRoot(),
     DatepickerModule.forRoot(),
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgMultiSelectDropDownModule.forRoot()
   ],
   declarations: [
     AppComponent,
     CrearPerfilComponent,
     AlumnoComponent,
     CarreraComponent,
-    EmpresaComponent
+    EmpresaComponent,
+    MateriaComponent
   ],
   bootstrap: [ AppComponent ]
 })
